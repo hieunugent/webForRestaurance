2df44be41665cff9e2485b362e5ee6208834cb1a
hieunugent <hieunugent@gmail.com>
2020-07-09 17:39:33 -0700

    fix server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './App.css';
-import NavBar from './components/Navbar.jsx'
+import NavBar from './components/navbar.jsx'
 import Image1 from './components/topimage.jsx'
 function App() {
   return (
